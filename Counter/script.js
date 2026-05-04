7b8c1d5dd5dbcb76fe13699d0b604b84ff1a2932
priyalpanara <priyalpanar12@gmail.com>
2026-05-05 00:15:53 +0530

    add Start and End Btns

--- Counter/script.js
+++ Counter/script.js
@@ -1,9 +1,12 @@
 let IncrementBtn=document.getElementById("IncrementBtn");
 let DecrementBtn=document.getElementById("DecrementBtn");
 let ResetBtn=document.getElementById("ResetBtn");
+let StartValue=document.getElementById("StartValue");
+let EndValue=document.getElementById("EndValue");
+let SetStartBtn=document.getElementById("SetStartBtn");
+let SetEndBtn=document.getElementById("SetEndBtn");
 
 let display=document.getElementById("display");
-
 
 
 IncrementBtn.addEventListener("click",function(){
@@ -12,10 +15,6 @@ IncrementBtn.addEventListener("click",function(){
   value++; 
   value=value<10? `0${value}`:value;
   display.innerText=value
-  
-
- 
-
 });
 
 DecrementBtn.addEventListener("click",function(){
@@ -24,12 +23,26 @@ DecrementBtn.addEventListener("click",function(){
     value--
     value=value<10 && value>0? `0${value}`:value;
     display.innerText=value
-
-
 });
 
 ResetBtn.addEventListener("click",function(){
     display.innerText=0;
 })
+
+
+SetStartBtn.addEventListener("click", function(){
+    let value=parseInt(StartValue.value);
+    display.innerText=value;
+})
+
+SetEndBtn.addEventListener("click", function(){
+    let value=parseInt(EndValue.value);
+    display.innerText<=value
+    if(display.innerText>value){
+        break;
+    }
+})
+
+
 
 
