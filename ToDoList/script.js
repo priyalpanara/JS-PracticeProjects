5a8ffcf95ae3f9f50e1051a5c5bff6558493c3b2
priyalpanara <priyalpanar12@gmail.com>
2026-05-17 19:11:03 +0530

    To-do App

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,14 +1,44 @@
 let taskInput=document.getElementById("taskInput");
-console.log(taskInput.value);
 let addTask=document.getElementById("addTask");
-let delTask=document.getElementById("delTask");
-// let completeTask=document.getElementById("completeTask");
+let taskContainer=document.getElementById("TaskListContainer")
 let taskList=[];
 
 addTask.addEventListener("click", function(){
-    console.log(taskInput.value)
-    let task=taskInput.innerText;
-        console.log(task);
-        taskList.push(task);
-        console.log(taskList);
+    let task=taskInput.value;
+    taskList.push({
+        tasktitle:task
+    })
+    addAllTasks();     
+});
+
+function addAllTasks(){
+    
+    taskContainer.innerHTML="";
+
+
+    for(let i=0; i<taskList.length; i++){
+    console.log(taskList[i]);
+
+    let checkbox=document.createElement("input");
+    checkbox.type="checkbox";
+
+    let li=document.createElement("li");
+    li.innerText=taskList[i].tasktitle;
+    li.prepend(checkbox);
+
+    let button=document.createElement("button");
+    button.innerText="Delete";
+    li.appendChild(button);
+    
+    button.addEventListener("click", function(){
+        li.remove();   
+        let taskremove=taskList.splice(i,1);
+        console.log(taskremove); 
     });
+
+
+    taskContainer.appendChild(li);
+    }
+    
+}
+
